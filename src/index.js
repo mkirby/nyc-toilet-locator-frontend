2447f7ce3ff8d076abaaeb6dbbbf016d03f3bfc7
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-09 20:22:28 -0500

    started ToiletComponent and index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import { client } from "./FetchClient.js"
 
+const main = document.querySelector("main")
+
+const initialize = () => {
+  client.get("/toilets")
+    .then(toiletArray => {
+      toiletArray.forEach(toiletObj => {
+        const toiletComponent = new ToiletComponent(toiletObj)
+        toiletComponent.render(main)
+      })
+    })
+}
+
+initialize()
